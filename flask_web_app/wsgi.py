ed22d5f5eb917beda41e5c022360f4b862fa58e6
Chetan Kapadia <kapadia_chetan@hotmail.com>
2025-11-24 23:40:24 -0500

    added flask web app for demo

---
+++ flask_web_app/wsgi.py
@@ -0,0 +1,6 @@
+import os
+from app import app
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000)) # Default to 5000 if PORT isn't set
+    app.run(host='0.0.0.0', port=port)
